534010a4c162d6f85b28fabe61f9c0bb659a30c4
shivam907 <shivamkaushal907@gmail.com>
2022-12-08 00:05:19 +0530

    An Update

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ const typed = new Typed("#type", {
   loop: true,
   //   fadeOut: true,
 });
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    const mouse = document.querySelector(".scroll-downs");
+    mouse.classList.toggle("op");
+    console.log($nav.height(), $(this).scrollTop() > $nav.height() / 3);
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height() / 3);
+  });
+});
+
+console.log("On Load");
+const loader = () => {
+  $(window).on("load", function () {
+    setTimeout(() => {
+      $(".loading").fadeIn(500, () => {
+        setTimeout(() => {
+          $(".loading").fadeOut(500);
+        }, 300);
+      });
+    }, 300);
+  });
+};
+loader();
