59705cfabe4df51560eabc15427e91f38c4a82a8
shivam907 <shivamkaushal907@gmail.com>
2022-12-07 22:00:06 +0530

    An Update

---
+++ script.js
@@ -0,0 +1,13 @@
+const typed = new Typed(".type", {
+  strings: [
+    "Software Developer",
+    "Frontend Developer",
+    "Web Designer",
+    "Backend Developer",
+    "App Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  fadeOut: true,
+});
